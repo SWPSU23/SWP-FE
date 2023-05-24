123f9db4f237b63a51046c3f3fc9931113d31089
votantai3070 <votantai.07092002@gmail.com>
2023-05-24 15:39:12 +0700

    Create menu component(CMS)

--- CMS/src/App.js
+++ CMS/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Menu from './components/menu/Menu';
 
 function App() {
-	return <div className="App">CMS</div>;
+	return (
+		<div className="app">
+			<Menu></Menu>
+		</div>
+	);
 }
 
 export default App;
