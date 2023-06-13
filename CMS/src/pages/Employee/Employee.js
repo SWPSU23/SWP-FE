90c2c310e9987c81f86543e78b1c2a9a92789b38
BaPhuocPham <phuoc.18112002@gmail.com>
2023-06-13 12:06:30 +0700

    create employee table component

--- CMS/src/pages/Employee/Employee.js
+++ CMS/src/pages/Employee/Employee.js
@@ -3,9 +3,9 @@ import './Employee.css';
 import {FormEmployee} from '../../components/FormEmployee/FormEmployee';
 import {Menu} from '../../components/MenuNavi/Menu';
 import {ActionBar} from '../../components/ActionBar/ActionBar';
-import ProductTable from '../../components/ProductTable/ProductTable';
 import Loading from '../../components/Loading/Loading';
 import Pagination from '../../components/Pagination/Pagination';
+import {EmployeeTable} from '../../components/EmployeeTable/EmployeeTable';
 
 export const Employee = () => {
 	// HANDLE LOADING
@@ -40,7 +40,7 @@ export const Employee = () => {
 				handleToggleForm={handleToggleForm}
 			/>
 			{openForm ? <FormEmployee handleToggleForm={handleToggleForm} /> : <div></div>}
-			{loading ? <Loading /> : <ProductTable handleToggleForm={handleToggleForm} />}
+			{loading ? <Loading /> : <EmployeeTable handleToggleForm={handleToggleForm} />}
 			<Pagination
 				currentPage={currentPage}
 				totalPages={totalPages}
