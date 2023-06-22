199e8792b412d4d6540e97682dde899cdad255eb
BaPhuocPham <phuoc.18112002@gmail.com>
2023-06-22 11:15:43 +0700

    create_sale_report_chart

---
+++ CMS/src/shared/RevenueData.js
@@ -0,0 +1,42 @@
+export const revenueData = [
+	{
+		id: 1,
+		year: 2014,
+		revenue: 7888,
+	},
+	{
+		id: 2,
+		year: 2015,
+		revenue: 54999,
+	},
+	{
+		id: 3,
+		year: 2016,
+		revenue: 25699,
+	},
+	{
+		id: 4,
+		year: 2017,
+		revenue: 56599,
+	},
+	{
+		id: 5,
+		year: 2018,
+		revenue: 7877,
+	},
+	{
+		id: 6,
+		year: 2019,
+		revenue: 9588,
+	},
+	{
+		id: 7,
+		year: 2020,
+		revenue: 79998,
+	},
+	{
+		id: 8,
+		year: 2021,
+		revenue: 99998,
+	},
+];
