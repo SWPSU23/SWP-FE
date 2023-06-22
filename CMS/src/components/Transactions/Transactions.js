4a29a0da77be71be2ee105fac44c949c6101df50
BaPhuocPham <phuoc.18112002@gmail.com>
2023-06-22 22:15:42 +0700

    create order report component

--- CMS/src/components/Transactions/Transactions.js
+++ CMS/src/components/Transactions/Transactions.js
@@ -1,6 +1,25 @@
-import React from 'react';
-import './Transactions.css';
+import React, {useState} from 'react';
+import {Line} from 'react-chartjs-2';
+import {bestSellingData} from '../../shared/BestSellingProductData';
+import {Chart as ChartJS} from 'chart.js/auto';
+import {orderData} from '../../shared/OrderData';
+import style from './Transactions.module.css';
 
 export const Transactions = () => {
-	return <div className="Transactions">Transactions</div>;
+	const [chartData, setChartData] = useState({
+		labels: orderData.map((data) => data.title),
+		datasets: [
+			{
+				label: 'Order Report',
+				data: orderData.map((data) => data.totalOrder),
+				backgroundColor: '#F765A3',
+			},
+		],
+	});
+	return (
+		<div className={style.transactions}>
+			<p className={style.title}>Order Report</p>
+			<Line data={chartData} className={style.lineChart} />
+		</div>
+	);
 };
