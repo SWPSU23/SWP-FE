4cd60c04c93642f445b0870fe0f6a44d4b58f998
votantai3070 <votantai.07092002@gmail.com>
2023-07-12 19:22:57 +0700

    create price total form

--- Sales/src/components/PriceTotal/PriceTotal.js
+++ Sales/src/components/PriceTotal/PriceTotal.js
@@ -2,5 +2,30 @@ import React from 'react';
 import './PriceTotal.css';
 
 export const PriceTotal = () => {
-	return <div>PriceTotal</div>;
+	return (
+		<div className="priceTotal">
+			<div className="inputForm">
+				<div className="inputTotal">
+					<label>Quantity:</label>
+					<input value="15" />
+				</div>
+				<div className="inputTotal">
+					<label>Product tax:</label>
+					<input value="0" />
+				</div>
+				<div className="inputTotal">
+					<label>Subtotal:</label>
+					<input value="300.000" />
+				</div>
+				<div className="inputTotal">
+					<label>Surcharge:</label>
+					<input value="0" />
+				</div>
+			</div>
+			<div className="totalForm">
+				<p>TOTAL</p>
+				<input value="300.000" />
+			</div>
+		</div>
+	);
 };
